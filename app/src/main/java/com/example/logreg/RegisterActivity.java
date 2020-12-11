4bf569a1c99898245cd2bd388ae6dbe5f9359081
Ádám Tilly <tilly.adam@gmail.com>
2020-12-11 14:43:03 +0100

    Register Activity regisztráció ellenőrzés

--- app/src/main/java/com/example/logreg/RegisterActivity.java
+++ app/src/main/java/com/example/logreg/RegisterActivity.java
@@ -3,8 +3,10 @@ package com.example.logreg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText EditText_RegisterEmail, EditText_RegisterFelhasznalonev, EditText_RegisterJelszo, EditText_RegisterTeljesnev;
@@ -15,6 +17,28 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         init();
+
+        Button_RegisterRegisztracio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (EditText_RegisterEmail.getText().toString().trim().length() == 0) {
+                    EditText_RegisterEmail.setError("Adjon meg e-mailt!");
+                    return;
+                }
+                if (EditText_RegisterFelhasznalonev.getText().toString().trim().length() == 0) {
+                    EditText_RegisterFelhasznalonev.setError("Adjon meg felhasználónevet!");
+                    return;
+                }
+                if (EditText_RegisterJelszo.getText().toString().trim().length() == 0) {
+                    EditText_RegisterJelszo.setError("Adjon meg jelszót!");
+                    return;
+                }
+                if (EditText_RegisterTeljesnev.getText().toString().trim().length() == 0) {
+                    EditText_RegisterTeljesnev.setError("Adja meg a teljesnevét!");
+                    return;
+                }
+            }
+        });
     }
 
     private void init() {
